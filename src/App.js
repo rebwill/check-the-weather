02091395b7eb7881cfe06f107163f94aa80ec1b0
rebwill <47540675+rebwill@users.noreply.github.com>
2019-12-20 12:48:56 -0500

    fetch functioning properly and navbar formatted

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Navbar from "./components/navbar";
+import "./styles/index.scss";
 // import logo from "./logo.svg";
 // import "./App.css";
 // import Weather from "./components/weather";
@@ -10,13 +12,26 @@ class App extends Component {
     this.state = {
       error: null,
       isLoaded: false,
-      weather: []
+      weather: [],
+      zipCode: 0
     };
   }
 
-  componentDidMount() {
+  handleChange = e => {
+    this.setState({ zipCode: e.target.value });
+  };
+
+  handleSubmit = e => {
+    // alert("A zipcode was submitted: " + this.state.zipCode);
+    e.preventDefault();
+    this.fetchWeather();
+  };
+
+  fetchWeather = () => {
+    let zipCodeParam = this.state.zipCode;
+    console.log(this.state.zipCode);
     fetch(
-      "http://api.weatherapi.com/v1/current.json?key=5b42e62dd43941e18fd195730191712&q=33139"
+      `http://api.weatherapi.com/v1/current.json?key=5b42e62dd43941e18fd195730191712&q=${zipCodeParam}`
     )
       .then(res => res.json())
       .then(
@@ -30,18 +45,86 @@ class App extends Component {
           console.log(error);
         }
       );
-  }
+  };
 
   render() {
     const { error, isLoaded, weather } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
-      return <div>Loading...</div>;
+      return (
+        <div>
+          <Navbar />
+          <div className="container">
+            <form onSubmit={this.handleSubmit} className="zipcode-form">
+              <div className="form-info">
+                Enter a US zipcode here to get current weather conditions.
+              </div>
+              <label>
+                US Zipcode:
+                <input
+                  type="text"
+                  value={this.state.zipCode}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+        </div>
+      );
+    } else if (isLoaded && weather) {
+      return (
+        <div>
+          <Navbar />
+          <div className="container">
+            <h1>Here's the weather you requested:</h1>
+            <p>
+              Temperature: {weather.temp_f} °F
+              <br />
+              Humidity: {weather.humidity}%<br />
+              Feels like: {weather.feelslike_f} F
+              <br />
+              Wind speed: {weather.wind_mph} mph
+              <br />
+              Wind gusts speed: {weather.gust_mph} mph
+              <br />
+              Wind direction: {weather.wind_dir}
+              <br />
+              Cloud cover: {weather.cloud}%<br />
+              Precipitation: {weather.precip_in} inches
+            </p>
+            <h1>Want to check another location?</h1>
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                US Zipcode:
+                <input
+                  type="text"
+                  value={this.state.zipCode}
+                  onChange={this.handleChange}
+                />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+        </div>
+      );
     } else {
       return (
         <div>
-          <p>Temp in F: {weather.temp_f}</p>
+          <Navbar />
+          <p>Please enter a valid US zipcode.</p>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              US Zipcode:
+              <input
+                type="text"
+                value={this.state.zipCode}
+                onChange={this.handleChange}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
         </div>
       );
     }
@@ -49,3 +132,5 @@ class App extends Component {
 }
 
 export default App;
+
+// {weather.temp_f}
---
+++ src/components/navbar.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <a className="navbar-brand" href="/">
+          What's the Weather?{" "}
+          <span role="img" aria-label="cloud emoji">
+            🌦️
+          </span>
+        </a>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item nav-about">
+              <a className="nav-link" href="/about">
+                ABOUT
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
