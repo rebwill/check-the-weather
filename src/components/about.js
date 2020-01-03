5dea088a826993c09e76a0fb8129ec2d84368bbd
rebwill <47540675+rebwill@users.noreply.github.com>
2020-01-03 11:43:28 -0500

    update

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ class About extends Component {
   render() {
     return (
       <div>
-        <h1>This is the about page.</h1>
+        <h1>This is the about page!</h1>
       </div>
     );
   }
