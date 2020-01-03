3f6d0a9f253e7af6a4779b64fb0df5c4b02c7bde
rebwill <47540675+rebwill@users.noreply.github.com>
2020-01-03 10:12:51 -0500

    removed /build from .gitignore

---
+++ build/precache-manifest.e94eed2f61f4e74c4d41e8ea26f75845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3791d74ae818362554f37520812a2593",
+    "url": "/index.html"
+  },
+  {
+    "revision": "775b93a5221e6bba0f47",
+    "url": "/static/css/main.cc4ec9ab.chunk.css"
+  },
+  {
+    "revision": "30a4c45a02046ddb7d90",
+    "url": "/static/js/2.c45dd3c1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c45dd3c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "775b93a5221e6bba0f47",
+    "url": "/static/js/main.7a9ecc9e.chunk.js"
+  },
+  {
+    "revision": "b6acf0e4cefbd6953a1b",
+    "url": "/static/js/runtime-main.f2eb5d68.js"
+  }
+]);
